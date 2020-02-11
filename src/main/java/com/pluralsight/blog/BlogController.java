82126be3ffdf3f55c24c0acad982aca79c309e8c
Nik <nn00317@surrey.ac.uk>
2020-02-11 19:50:50 +0000

    Added single post page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -7,6 +7,7 @@ import com.pluralsight.blog.model.Post;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,6 +18,13 @@ public class BlogController {
     public BlogController(PostRepository _postRepository) {
         super();
         postRepository = _postRepository;
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+        Post post = postRepository.findById(id);
+        modelMap.put("post", post);
+        return "post-details";
     }
 
     @RequestMapping("/")
