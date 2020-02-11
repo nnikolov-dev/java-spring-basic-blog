9fb853e724b1d39e22cc39abf0d245534d1ca896
Nik <nn00317@surrey.ac.uk>
2020-02-11 19:27:17 +0000

    Modified the BlogController

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,5 +1,10 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository = null;
+
+    public BlogController(PostRepository _postRepository) {
+        super();
+        postRepository = _postRepository;
+    }
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
+        List<Post> allPosts = postRepository.getAllPosts(); 
+        modelMap.put("posts", allPosts);
         return "home";
     }
 }
